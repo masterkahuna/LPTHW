ee99bcc94fca27d8262696515d375430fe75498d
Sola Soyombo <olusolas@gmail.com>
2014-12-24 00:34:07 -0800

    Adding exercise files

---
+++ Python/ex11.py
@@ -0,0 +1,12 @@
+print "How old are you?"
+age = raw_input()
+print "How tall are you?"
+height = raw_input()
+print "How much do you weight?"
+weight = raw_input()
+
+print "So, you are %r years old, %r tall and %r heavy." % (age, height, weight)
+
+print "2nd attempt"
+
+print "So, you are %s years old, %s tall and %s heavy." % (age, height, weight)
---
+++ Python/ex9.py
@@ -0,0 +1,16 @@
+# Here's some new stuff, remember type it exactly
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJune\nJuly\nAug"
+weeks = "The weeks are %s %s %s %s"
+
+print weeks % ("eni", "eji", "eta", "erin")
+print "Here are the day: ", days
+print "Here are the months: ", months
+
+print """
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+"""
